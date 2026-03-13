a7c11adba248e5fbdff2210a001fcdb30d1949a7
Lê Quỳnh Minh Vân <vanlequynhminh11l421@gmai.com>
2026-03-13 23:10:31 +0700

    done

--- src/main/java/vn/edu/sp_demo333/controller/StudentController.java
+++ src/main/java/vn/edu/sp_demo333/controller/StudentController.java
@@ -1,33 +1,40 @@
 package vn.edu.sp_demo333.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.sp_demo333.entity.Students;
-import vn.edu.sp_demo333.repository.StudentRepository;
+import vn.edu.sp_demo333.service.StudentService;
 import java.util.List;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
-    private final StudentRepository repository;
+    private final StudentService service;
 
-    public StudentController(StudentRepository repository) {
-        this.repository = repository;
+    public StudentController(StudentService service) {
+        this.service = service;
     }
 
     @GetMapping
-    public List<Students> getAllStudent() {
-        return repository.findAll();
+    public List<Students> getAll() {
+        return service.getAll();
     }
 
     @PostMapping
-    public Students addStudent(@Valid @RequestBody Students student) {
-        return repository.save(student);
+    public Students add(@Valid @RequestBody Students student) {
+        return service.add(student);
     }
 
     @GetMapping("/{id}")
-    public Students getStudentById(@PathVariable Long id) {
-        return repository.findById(id).orElse(null);
+    public Students getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.ok("Deleted student id: " + id);
     }
 }
